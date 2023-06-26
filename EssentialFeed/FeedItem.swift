93be0f17cb102887707eb8c31e8f29b58508e14e
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-26 15:18:48 -0400

    create the FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sonic on 26/6/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
