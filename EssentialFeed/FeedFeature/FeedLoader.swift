7870d0e439c20b818adc6e9267ef003e9f180b3f
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-26 20:43:45 -0400

    Remove the generic error type form the LoadFeedResult to simplify type constraints

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum LoadFeedResult<Error: Swift.Error> {
+public enum LoadFeedResult {
     case success([FeedItem])
     case failure(Error)
 }
@@ -15,6 +15,6 @@ public enum LoadFeedResult<Error: Swift.Error> {
 protocol FeedLoader {
     associatedtype Error: Swift.Error
     
-    func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
+    func load(completion: @escaping (LoadFeedResult) -> Void)
 }
 
