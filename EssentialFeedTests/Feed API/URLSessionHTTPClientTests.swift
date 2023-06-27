32501fa152adf0c3dad72a3a353c30a1f73e6658
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-26 22:11:14 -0400

    Add nil data and nil response to the request error test case to make the setup more explicit

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -27,10 +27,10 @@ class URLSessionHTTPClient {
 class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_failsOnRequestError() {
-         URLProtocolStub.startInterceptingRequests()
-         let url = URL(string: "http://any-url.com")!
-         let error = NSError(domain: "any error", code: 1)
-         URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "http://any-url.com")!
+        let error = NSError(domain: "any error", code: 1)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
         let sut = URLSessionHTTPClient()
 
@@ -38,7 +38,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
         sut.get(from: url) { result in
             
-            switch result {
+        switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
                 XCTAssertEqual(receivedError.code, error.code)
@@ -57,11 +57,13 @@ class URLSessionHTTPClientTests: XCTestCase {
         private static var stubs = [URL: Stub]()
         
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error? = nil) {
-            stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func startInterceptingRequests() {
@@ -85,6 +87,14 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         override func startLoading() {
             guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
 
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
