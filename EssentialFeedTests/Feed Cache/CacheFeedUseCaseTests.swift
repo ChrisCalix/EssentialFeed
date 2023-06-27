236741c43337a97e69202429167e9d4d1982634e
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-27 16:07:31 -0400

    Save command request cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,18 +6,29 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
-class CacheFeedUseCaseTests: XCTest {
+class CacheFeedUseCaseTests: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
@@ -25,5 +36,23 @@ class CacheFeedUseCaseTests: XCTest {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
