aded452a5cd20cc9fdad43d60df8eae12a36143f
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-28 11:20:44 -0400

    Move FeedCachePolicy to its own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,26 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Sonic on 28/6/23.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
