ab7547211cda22af925da9367c72b4d307d66941
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-27 21:11:20 -0400

    Load command deletes cache on retrieval error

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -35,6 +35,7 @@ public final class LocalFeedLoader {
         store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
+                self.store.deleteCacheFeed { _ in }
                 completion(.failure(error))
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
