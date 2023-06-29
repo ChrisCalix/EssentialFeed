4fecf9596b884334819bba002f9bcf24aaee5fc9
christian.sonic <yoh.chrisitan@gmail.com>
2023-06-29 12:50:31 -0400

    Nest LoadFeedResult into the FeedLoader protocol as `FeedLoader.Result` since they’re closely related

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-public typealias LoadFeedResult = Result<[FeedImage], Error>
 
 public protocol FeedLoader {
+    typealias Result = Swift.Result<[FeedImage], Error>
     
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+    func load(completion: @escaping (Result) -> Void)
 }
 
--- EssentialFeedAPIEntToEndTests/EssentialFeedAPIEntToEndTests.swift
+++ EssentialFeedAPIEntToEndTests/EssentialFeedAPIEntToEndTests.swift
@@ -31,7 +31,7 @@ final class EssentialFeedAPIEntToEndTests: XCTestCase {
     
 //MARK: - Helpers
     
-    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
@@ -40,7 +40,7 @@ final class EssentialFeedAPIEntToEndTests: XCTestCase {
         
         let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult : LoadFeedResult?
+        var receivedResult : FeedLoader.Result?
         loader.load { result in
             receivedResult = result
             exp.fulfill()
