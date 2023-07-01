f566e33f9fffca36ab94692cef8c982402d20b9a
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-01 15:35:58 -0400

    Move `FeedImageDataLoader` to its own file

---
+++ EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Sonic on 1/7/23.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
