d5ae1e2191941f6c9fceaf1c8d9da0ebbf12412a
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-01 16:19:43 -0400

    Move `FeedLoader` loading state management to `FeedViewModel`. Now the `FeedRefreshViewConrtoller` acts a binder between the `View` and the `ViewModel`

---
+++ EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Sonic on 1/7/23.
+//
+
+import Foundation
+import EssentialFeed
+
+final class FeedViewModel {
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onChange: ((FeedViewModel) -> Void)?
+    var onFeedLoad: (([FeedImage]) -> Void)?
+    
+    private(set) var isLoading: Bool = false {
+        didSet {
+            onChange?(self)
+        }
+    }
+    
+    func loadFeed() {
+        isLoading = true
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onFeedLoad?(feed)
+            }
+            self?.isLoading = false
+        }
+    }
+}
