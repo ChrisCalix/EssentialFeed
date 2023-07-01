dee515664d46c7b0f68145d195c028e176cd298e
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-01 16:05:51 -0400

    Extract dependency creation and composition logic from `FeedViewController` into the new `FeedUIComposer`

--- EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -49,7 +49,7 @@ final class FeedImageCellController {
         task = imageLoader.loadImageData(from: model.url, completion: { _ in })
     }
     
-    deinit {
+    func cancelLoad() {
         task?.cancel()
     }
 }
