57a697619a2158dfbd1051f774cae846ad613220
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-02 18:08:18 -0400

    Move title configuration from `FeedViewController` to the `FeedUIComposer` - the View Controllers can be agnostic of Presenters if we move the configuration to composers

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -24,7 +24,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = FeedPresenter.title
         refresh()
     }
     
