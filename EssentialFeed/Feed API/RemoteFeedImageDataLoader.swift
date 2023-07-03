0c0986618484d014d545381a889a46e10dbee553
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-03 16:13:14 -0400

    Make `RemoteFeedImageDataLoader` conform to `FeedImageDataLoader`

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class RemoteFeedImageDataLoader {
+public class RemoteFeedImageDataLoader: FeedImageDataLoader {
     private let client: HTTPClient
     
     public init (client: HTTPClient) {
