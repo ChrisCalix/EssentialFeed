0252ee049abc3dfefd9c91e82e28ecd446ca7c35
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-03 16:33:04 -0400

    Extract duplicate base URL creation into a factory helper

--- EssentialFeedAPIEntToEndTests/EssentialFeedAPIEntToEndTests.swift
+++ EssentialFeedAPIEntToEndTests/EssentialFeedAPIEntToEndTests.swift
@@ -43,7 +43,7 @@ final class EssentialFeedAPIEntToEndTests: XCTestCase {
 //MARK: - Helpers
     
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let testServerURL = feedTestServerURL
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
@@ -62,7 +62,7 @@ final class EssentialFeedAPIEntToEndTests: XCTestCase {
     }
     
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeaks(client, file: file, line: line)
@@ -70,7 +70,7 @@ final class EssentialFeedAPIEntToEndTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
         
         var receivedResult: FeedImageDataLoader.Result?
-        _ = loader.loadImageData(from: testServerURL) {
+        _ = loader.loadImageData(from: url) {
             result in
             receivedResult = result
             exp.fulfill()
@@ -79,6 +79,10 @@ final class EssentialFeedAPIEntToEndTests: XCTestCase {
         wait(for: [exp], timeout: 30.0)
         
         return receivedResult
+    }
+    
+    private var feedTestServerURL: URL {
+        return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
     }
     
     private func expectedImage(at index: Int) -> FeedImage {
