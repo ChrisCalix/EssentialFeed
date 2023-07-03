344927eec5b6864883c7132cb349ddb23999866e
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-02 20:15:58 -0400

    Move main thread dispatch to the Composition layer with decorator

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,13 +12,47 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
-        let bundle = Bundle(for: FeedViewController.self)
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader:
+                     MainQueueDispatchDecorator(decoratee: feedLoader))
+
+        let feedController = FeedViewController.makeWith(
+         delegate: presentationAdapter,
+        title: FeedPresenter.title)
+        presentationAdapter.presenter = FeedPresenter(
+        feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
+        loadingView: WeakRefVirtualProxy(feedController))
+
+        return feedController
+    }
+}
+
+private final class MainQueueDispatchDecorator: FeedLoader {
+     private let decoratee: FeedLoader
+
+     init(decoratee: FeedLoader) {
+         self.decoratee = decoratee
+     }
+
+     func load(completion: @escaping (FeedLoader.Result) -> Void) {
+         decoratee.load { result in
+             if Thread.isMainThread {
+                 completion(result)
+             } else {
+                 DispatchQueue.main.async {
+                     completion(result)
+                 }
+             }
+         }
+     }
+ }
+
+private extension FeedViewController {
+     static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        feedController.delegate = presentationAdapter
-        feedController.title = FeedPresenter.title
-        presentationAdapter.presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader), loadingView: WeakRefVirtualProxy(feedController))
+        feedController.delegate = delegate
+        feedController.title = title
         return feedController
     }
 }
