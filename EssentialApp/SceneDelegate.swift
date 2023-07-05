e0109261052a6c9bc1cb2e7d3914a0b3f8708c91
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-05 17:28:50 -0400

    Add EssentialApp project

---
+++ EssentialApp/SceneDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  SceneDelegate.swift
+//  EssentialApp
+//
+//  Created by Sonic on 5/7/23.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5d1c78f21e661a0001ce7cfd/1562147059075/feed-case-study-v1-api-feed.json")!
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let feedLoader = RemoteFeedLoader(url: url, client: client)
+        let imageLoader = RemoteFeedImageDataLoader(client: client)
+        
+        let feedViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: feedLoader,
+            imageLoader: imageLoader)
+        
+        window?.rootViewController = feedViewController
+    }
+
+
+}
+
