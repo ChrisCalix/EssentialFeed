f1062509ea0d9dbc945b1753101d33c44a39087f
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-05 20:51:29 -0400

    Add UI Test target for runing high-level Acceptance Tests

---
+++ EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Sonic on 5/7/23.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+   
+}
