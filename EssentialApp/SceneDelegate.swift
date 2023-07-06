02e83876cbf9cb0d30ec0895b31d1f7cb3d815fc
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-06 14:59:19 -0400

    Remove unnecesary Main storyboard - the initial setup happens in code

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -35,7 +35,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
         configureWindow()
     }
     
@@ -60,6 +62,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: FeedImageDataLoaderCacheDecorator(
                         decoratee: remoteImageLoader,
                         cache: localImageLoader))))
+        
+        window?.makeKeyAndVisible()
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
--- EssentialAppTests/SceneDelegateTests.swift
+++ EssentialAppTests/SceneDelegateTests.swift
@@ -10,6 +10,16 @@ import EssentialFeediOS
 @testable import EssentialApp
 
 class SceneDelegateTests: XCTestCase {
+    
+    func test_configurationWindow_setsWindowAskeyAndVisible() {
+        let window = UIWindowSpy()
+          let sut = SceneDelegate()
+          sut.window = window
+          sut.configureWindow()
+        
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+    }
+    
     func test_sceneWillConnectToSession_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
@@ -22,5 +32,14 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topcontroller is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topcontroller)) instead")
+    }
+    
+    private class UIWindowSpy: UIWindow {
+        
+      var makeKeyAndVisibleCallCount = 0
+        
+      override func makeKeyAndVisible() {
+        makeKeyAndVisibleCallCount = 1
+      }
     }
 }
