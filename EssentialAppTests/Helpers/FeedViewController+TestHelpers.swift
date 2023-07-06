f0c0dc87dccc227f64a8c47dbea18117cef9b766
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-06 00:46:01 -0400

    Displays remote feed on launch when customer has connectivity

--- EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -42,6 +42,9 @@ extension FeedViewController {
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
     
+    func renderedFeedImageData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
     var errorMessage: String? {
         return errorView?.message
     }
