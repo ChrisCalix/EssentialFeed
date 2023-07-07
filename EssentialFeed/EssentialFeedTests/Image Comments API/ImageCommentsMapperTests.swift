e9557eddc8dfa77a678805eb0f203bc8ea5fafbe
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-07 00:26:22 -0400

    Move test helpers to shared scope

--- EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
+class ImageCommentsMapperTests: XCTestCase {
     
     func test_load_deliversErrorOnNon2xxHTTPResponse() {
         let (sut, client) = makeSUT()
