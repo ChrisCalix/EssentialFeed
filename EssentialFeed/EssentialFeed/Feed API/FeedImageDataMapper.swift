fbb085b31433a1604b796a8133764a2de9cdb00a
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-07 01:23:30 -0400

    Test `FeedImageDataMapper` in isolation

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Sonic on 7/7/23.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
