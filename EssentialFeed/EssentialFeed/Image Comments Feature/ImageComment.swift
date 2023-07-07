4a2486183325e643fd237114345cb63f8859c7a7
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-06 21:14:16 -0400

    Add ImageComment data model

---
+++ EssentialFeed/EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Sonic on 6/7/23.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
