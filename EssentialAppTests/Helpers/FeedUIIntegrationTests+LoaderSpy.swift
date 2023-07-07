e74275795a8c1b10d30bce961ff37744d066f209
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-07 01:13:29 -0400

    Remove `FeedLoader` protocol as we don’t need it anymore - we’re composing the types with universal abstractions provided by the Combine framework

--- EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -8,26 +8,30 @@
 import Foundation
 import EssentialFeed
 import EssentialFeediOS
+import Combine
 
 extension FeedUIIntegrationTests {
-    class LoaderSpy: FeedLoader, FeedImageDataLoader {
-        private var feedRequests = [(FeedLoader.Result) -> Void]()
+    class LoaderSpy: FeedImageDataLoader {
+        
+        private var feedRequests = [PassthroughSubject<[FeedImage], Error>]()
         
         var loadFeedCallCount: Int {
             return feedRequests.count
         }
 
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            feedRequests.append(completion)
+        func loadPublisher() -> AnyPublisher<[FeedImage], Error> {
+            let publisher = PassthroughSubject<[FeedImage], Error>()
+            feedRequests.append(publisher)
+            return publisher.eraseToAnyPublisher()
         }
         
         func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
-            feedRequests[index](.success(feed))
+            feedRequests[index].send(feed)
         }
         
         func completeFeedLoadingWithError(at index: Int = 0) {
             let error = NSError(domain: "an error", code: 0)
-            feedRequests[index](.failure(error))
+            feedRequests[index].send(completion: .failure(error))
         }
         
         // MARK: - FeedImageDataLoader
