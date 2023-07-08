26f15c678bc6d7b10290e949090fef6af285d69b
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-08 15:39:03 -0400

    Replace `FeedViewControllerDelegate` with a closure

--- EssentialApp/FeedUIComposer.swift
+++ EssentialApp/FeedUIComposer.swift
@@ -21,9 +21,8 @@ public final class FeedUIComposer {
         ) -> ListViewController {
         let presentationAdapter = FeedPresentationAdapter(loader: feedLoader)
 
-        let feedController = makeFeedViewController(
-         delegate: presentationAdapter,
-        title: FeedPresenter.title)
+        let feedController = makeFeedViewController(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
         presentationAdapter.presenter = LoadResourcePresenter(
         resourceView: FeedViewAdapter(
             controller: feedController,
@@ -35,11 +34,10 @@ public final class FeedUIComposer {
         return feedController
     }
 
-    private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> ListViewController {
-         let bundle = Bundle(for: ListViewController.self)
+    private static func makeFeedViewController(title: String) -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! ListViewController
-        feedController.delegate = delegate
         feedController.title = title
         return feedController
     }
--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ListViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 import EssentialFeed
 
-public protocol FeedViewControllerDelegate {
-    func didRequestFeedRefresh()
-}
-
 public protocol CellController {
     func view(in tableView: UITableView) -> UITableViewCell
     func preload()
@@ -30,7 +26,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         }
     }
     
-    public var delegate: FeedViewControllerDelegate?
+    public var onRefresh: (() -> Void)?
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,7 +41,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     }
     
     @IBAction private func refresh() {
-        delegate?.didRequestFeedRefresh()
+        onRefresh?()
     }
     
     public func display(_ cellController: [CellController]) {
