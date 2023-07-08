25f07bf5ec2e31f49cb8bb8a39c22defb2c2d42d
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-08 16:20:39 -0400

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Sonic on 8/7/23.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,61 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Sonic on 8/7/23.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+    
+    func test_listWithComments() {
+        let sut = makeSUT()
+        
+        sut.display(comments())
+        
+        record(snapshot: sut.snapshot(for: .iPhone14Pro(style: .light)), named: "IMAGE_COMMENTS_light")
+        record(snapshot: sut.snapshot(for: .iPhone14Pro(style: .dark)), named: "IMAGE_COMMENTS_dark")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func comments() -> [CellController] {
+        return [
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                    date: "1000 years ago",
+                    username: "a long long long long username"
+                )
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "East Side Gallery\nMemorial in Berlin, Germany",
+                    date: "10 days ago",
+                    username: "a username"
+                )
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "nice",
+                    date: "1 hour ago",
+                    username: "a."
+                )
+            )
+        ]
+    }
+}
