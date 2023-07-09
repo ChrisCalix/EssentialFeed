d80ad4e82d9f210a09d4aa1eeafcc4cb3ac1ac23
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-09 17:15:05 -0400

    Remove duplication

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -32,7 +32,7 @@ extension CoreDataFeedStore: FeedStore {
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in
             completion(Result {
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                try ManagedCache.deleteCache(in: context)
             })
         }
     }
