d1ba497763918fa47a42e14d397bc1b69adfd6ba
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-09 12:20:33 -0400

    Make feedLoader closure return `Paginated<FeedImage>`

--- EssentialApp/FeedUIComposer.swift
+++ EssentialApp/FeedUIComposer.swift
@@ -13,10 +13,10 @@ import EssentialFeediOS
 public final class FeedUIComposer {
     private init() {}
     
-    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
     
     public static func feedComposedWith(
-             feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>,
+             feedLoader: @escaping () -> AnyPublisher<Paginated<FeedImage>, Error>,
              imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
              selection: @escaping (FeedImage) -> Void = { _ in }
         ) -> ListViewController {
@@ -31,7 +31,7 @@ public final class FeedUIComposer {
             selection: selection),
         loadingView: WeakRefVirtualProxy(feedController),
         errorView: WeakRefVirtualProxy(feedController),
-        mapper: FeedPresenter.map)
+        mapper: { $0 })
 
         return feedController
     }
--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -76,7 +76,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
     
-    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error> {
+    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher< Paginated<FeedImage>, Error> {
         let url = FeedEndpoint.get.url(baseURL: baseURL)
 
         return httpClient
@@ -84,6 +84,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .tryMap(FeedItemsMapper.map)
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
+            .map {
+                Paginated(items: $0)
+            }
+            .eraseToAnyPublisher()
     }
             
     private func makeLocalImageLoaderWithRemoteFallback(url: URL) -> FeedImageDataLoader.Publisher {
