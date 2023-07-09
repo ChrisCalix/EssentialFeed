abc19498e6e2cfe94ac483f88848f6ccdf3ed917
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-08 20:58:59 -0400

    Move localization helpers to shared scope

--- EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -24,3 +24,19 @@ func anyURL() -> URL {
 func uniqueFeed() -> [FeedImage] {
     return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
 }
+
+private class DummyView: ResourceView {
+    func display(_ viewModel: Any) {}
+}
+
+var loadError: String {
+    LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    FeedPresenter.title
+}
+
+var commentsTitle: String {
+    ImageCommentsPresenter.title
+}
