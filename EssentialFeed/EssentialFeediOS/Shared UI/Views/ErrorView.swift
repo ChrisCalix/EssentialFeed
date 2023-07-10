88f823c37e2f6c2d4f1550a513f41643cc77b346
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-09 22:11:07 -0400

    Update to Xcode 14.0 / iOS 16.0
    Configure error view with the new UIButton.Configuration APIs

--- EssentialFeed/EssentialFeediOS/Shared UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Views/ErrorView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public final class ErrorView: UIButton {
     public var message: String? {
-        get { return isVisible ? title(for: .normal) : nil }
+        get { return isVisible ? configuration?.title : nil }
         set { setMessageAnimated(newValue) }
     }
 
@@ -24,21 +24,27 @@ public final class ErrorView: UIButton {
         super.init(coder: coder)
     }
 
-    private func configure() {
-        backgroundColor = .errorBackgroundColor
+    private var titleAttributes: AttributeContainer {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.center
 
-        addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
-        configureLabel()
-        hideMessage()
+        var attributes = AttributeContainer()
+        attributes.paragraphStyle = paragraphStyle
+        attributes.font = UIFont.preferredFont(forTextStyle: .body)
+        return attributes
     }
 
-    private func configureLabel() {
-        titleLabel?.textColor = .white
-        titleLabel?.textAlignment = .center
-        titleLabel?.numberOfLines = 0
-        titleLabel?.font = .preferredFont(forTextStyle: .body)
-        
-        titleLabel?.adjustsFontForContentSizeCategory = true
+    private func configure() {
+        var configuration = Configuration.plain()
+        configuration.titlePadding = 0
+        configuration.baseForegroundColor = .white
+        configuration.background.backgroundColor = .errorBackgroundColor
+        configuration.background.cornerRadius = 0
+        self.configuration = configuration
+
+        addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
+
+        hideMessage()
     }
 
     private var isVisible: Bool {
@@ -54,8 +60,10 @@ public final class ErrorView: UIButton {
     }
 
     private func showAnimated(_ message: String) {
-        setTitle(message, for: .normal)
-        self.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+        configuration?.attributedTitle = AttributedString(message, attributes: titleAttributes)
+
+        configuration?.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+
         UIView.animate(withDuration: 0.25) {
             self.alpha = 1
         }
@@ -63,18 +71,17 @@ public final class ErrorView: UIButton {
 
     @objc private func hideMessageAnimated() {
         UIView.animate(
-            withDuration: 0.25,
-            animations: { self.alpha = 0 },
-            completion: { completed in
-                if completed { self.hideMessage() }
-            }
-        )
+        withDuration: 0.25,
+        animations: { self.alpha = 0 },
+        completion: { completed in
+            if completed { self.hideMessage() }
+        })
     }
 
     private func hideMessage() {
-        setTitle(nil, for: .normal)
         alpha = 0
-        self.configuration?.contentInsets = NSDirectionalEdgeInsets(top: -2.5, leading: 0, bottom: -2.5, trailing: 0)
+        configuration?.attributedTitle = nil
+        configuration?.contentInsets = .zero
         onHide?()
     }
 }
